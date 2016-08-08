bb7c71d9b2e438e0f1e17ca0102f1ad3a84bdc6b
pandi123 <pandikumar201295@gmail.com>
2016-08-08 00:13:59 -0700

    la.java
    afd

---
+++ la.java
@@ -0,0 +1,11 @@
+public class test
+{
+public static void main(String []args)
+{
+String s="mani";
+StringBuffer n=new StringBuffer(s);
+StringBuffer b=n.reverse();
+String a=b.toString();
+System.out.println(a);
+}
+}
